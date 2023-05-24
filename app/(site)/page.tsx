9c305b34ba62fb874aac0ffe4e34e09153fddadc
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-05-24 09:03:01 +0700

    update-Styles&ConfigApp

---
+++ app/(site)/page.tsx
@@ -0,0 +1,39 @@
+"use client"
+
+import React from 'react';
+import Image from 'next/image';
+
+// AddComponents
+// import AuthForm from './components/AuthForm';
+
+const Auth = () => {
+  return (
+    <>
+      <div
+        className='flex min-h-full flex-col justify-center
+        py-12 sm:px-6 lg:px-8 font-inter bg-primary-600'
+      >
+        <div className='sm:mx-auto sm:w-full sm:max-w-md '>
+          <Image
+            src="/images/logo.png"
+            width="48"
+            className="mx-auto w-auto"
+            height="48"
+            alt="Logo"
+          />
+          <h2
+            className="mt-6 text-center text-3xl
+            font-bold tracking-tight text-white font-inter
+            bg-gradient-to-r from-purple-500 to-blue-500
+            text-transparent bg-clip-text"
+          >
+            Sign in to your account
+          </h2>
+        </div>
+        {/* <AuthForm /> */}
+      </div>
+    </>
+  );
+};
+
+export default Auth;
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,30 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      backgroundColor: {
+        'gray-300': '#a3a3a3',
+        'gray-400': '#858585',
+        'primary-400': '#35596c',
+        'primary-500': '#0f2936',
+        'primary-600': '#091d2b',
+        'primary-700': '#051926',
+        'primary-800': '#060f14',
+        'secondary-300': '#7ac5d7',
+        'secondary-500': '#219ebc',
+        'secondary-800': '#00b5e0',
+        'tertiary-50': '#fff8e6',
+        'tertiary-100': '#fff1cd',
+        'quaternary-500': '#fb8500',
+      },
+      fontFamily: {
+        satoshi: ['Satoshi', 'sans-serif'],
+        inter: ['Inter', 'sans-serif'],
       },
     },
   },
-  plugins: [],
-}
+  plugins: [
+    // require('@tailwindcss/forms')({
+    //   strategy: 'class'
+    // })
+  ],
+};
