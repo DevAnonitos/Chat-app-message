cc3d0676f827b10b152893dc26616b05ec9d393d
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-05-26 20:03:07 +0700

    update-AddFooterItems

--- app/components/sidebar/MobileFooter.tsx
+++ app/components/sidebar/MobileFooter.tsx
@@ -1,8 +1,28 @@
+"use client"
+
 import React from 'react';
+import useConversation from '@/app/hooks/useConversation';
+import useRoutes from '@/app/hooks/useRoutes';
 
 const MobileFooter = () => {
+
+    const routes = useRoutes();
+    const { isOpen } = useConversation();
+
+    if(isOpen) {
+        return null;
+    }
+
     return (
-        <div>MobileFooter</div>
+        <>
+            <div
+                className='fixed justify-between w-full bottom-0
+                z-40 flex items-center bg-primary-700
+                border-t-[1px] lg:hidden'
+            >
+                div
+            </div>
+        </>
     );
 };
 
--- app/components/sidebar/MobileItem.tsx
+++ app/components/sidebar/MobileItem.tsx
@@ -1,1 +1,16 @@
+import React from "react";
+import Link from "next/link";
+import clsx from "clsx";
 
+interface MobileItemProps {
+
+}
+
+const MobileItem: React.FC<MobileItemProps> = () => {
+    return (
+        <>
+        </>
+    );
+};
+
+export default MobileItem;
