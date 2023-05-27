d3bbc292331221f777ff77cd231748c1137d90da
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-05-27 16:54:16 +0700

    update-useActiveList

---
+++ app/hooks/useActiveList.ts
@@ -0,0 +1,23 @@
+import { create } from "zustand";
+
+interface ActiveStore {
+    members: string[];
+    add: (id: string) => void;
+    remove: (id: string) => void;
+    set: (ids: string[]) => void;
+}
+
+const useActiveList = create<ActiveStore>((set) => ({
+    members: [],
+    add: (id) => set((state) => ({
+        members: [...state.members, id],
+    })),
+    remove: (id) => set((state) => ({
+        members: state.members.filter((memberId) => memberId !== id),
+    })),
+    set: (ids) => set({
+        members: ids,
+    }),
+}));
+
+export default useActiveList;
