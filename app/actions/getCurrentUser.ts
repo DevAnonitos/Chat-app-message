5dff3bc6ab690c45f1e66ca53bce4a962002165c
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-05-27 14:19:23 +0700

    update-getCurrentUser

---
+++ app/actions/getCurrentUser.ts
@@ -0,0 +1,30 @@
+import getSession from "./getSession";
+import prisma from "../libs/prismadb";
+
+import React from 'react'
+
+const getCurrentUser = async () => {
+    try {
+        const session = await getSession();
+
+        if(!session?.user?.email) {
+            return null;
+        }
+
+        const currentUser = await prisma.user.findUnique({
+            where: {
+                email: session.user.email as string,
+            },
+        });
+
+        if(!currentUser) {
+            return null;
+        }
+        
+    } catch (error: any) {
+        console.log(error);
+        return null;
+    }
+};
+
+export default getCurrentUser;
