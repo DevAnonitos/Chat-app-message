38fd472a43aa83617e67cbb28e8b74145bd9cd16
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-05-27 23:36:47 +0700

    update-FixUseActiveList

--- app/hooks/useActiveList.ts
+++ app/hooks/useActiveList.ts
@@ -1,22 +1,22 @@
-import { create } from "zustand";
+import { create } from 'zustand'
 
-interface ActiveStore {
+interface ActiveListStore {
     members: string[];
     add: (id: string) => void;
     remove: (id: string) => void;
     set: (ids: string[]) => void;
 }
 
-const useActiveList = create<ActiveStore>((set) => ({
+const useActiveList = create<ActiveListStore>((set) => ({
     members: [],
     add: (id) => set((state) => ({
-        members: [...state.members, id],
+        members: [...state.members, id]
     })),
     remove: (id) => set((state) => ({
-        members: state.members.filter((memberId) => memberId !== id),
+        members: state.members.filter((memberId) => memberId !== id)
     })),
     set: (ids) => set({
-        members: ids,
+        members: ids
     }),
 }));
 
