01c054eecf7ad227d3a662ea90cfe50b0e5c9514
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-05-28 21:11:29 +0700

    update-AddUsers

--- app/components/sidebar/DesktopSidebar.tsx
+++ app/components/sidebar/DesktopSidebar.tsx
@@ -24,7 +24,7 @@ const DesktopSidebar: React.FC<DesktopSidebarProps> = ({
                 className='hidden lg:fixed lg:inset-y-0
                 lg:left-0 lg:z-40 lg lg:w-20 xl:px-6
                 lg:overflow-y-auto lg:bg-primary-700 lg:border-r-[1px]
-                lg:pb-4 lg:flex lg:flex-col justify-between'
+                lg:pb-4 lg:flex lg:flex-col justify-between border-gray-500'
             >
                 <nav className='mt-4 flex flex-col justify-between'>
                     <ul
--- app/users/components/UserList.tsx
+++ app/users/components/UserList.tsx
@@ -2,6 +2,7 @@
 
 import React from "react";
 import { User } from "@prisma/client";
+import { AiOutlineLink } from "react-icons/ai";
 
 interface UserListProps {
     items: User[];
@@ -13,9 +14,25 @@ const UserList: React.FC<UserListProps> = ({
     return (
         <>
             <aside
-                className=""
+                className="fixed inset-y-0 pb-20 lg:pb-0
+                lg:left-20 lg:w-80 lg:block overflow-y-auto
+                border-r border-gray-500 block w-full left-0"
             >
-
+                <div className="px-5">
+                    <div className="flex-col">
+                        <div
+                            className="text-2xl font-bold
+                            text-[#fff8e6] py-4 flex
+                            items-center hover:underline cursor-pointer"
+                        >
+                            <AiOutlineLink
+                                className="w-6 h-6 mr-1"
+                            />
+                            Users
+                        </div>
+                    </div>
+                    {}
+                </div>
             </aside>
         </>
     );
