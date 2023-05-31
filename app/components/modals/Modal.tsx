fc42760ad0f5c8ba3a115e0fb06a81230c7c230f
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-06-01 00:32:39 +0700

    update-Add-Modal

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -17,6 +17,35 @@ const Modal: React.FC<ModalProps> = ({
 }) => {
     return (
         <>
+            <Transition.Root
+                show={isOpen}
+                as={Fragment}
+            >
+                <Dialog
+                    as="div"
+                    className="relative z-50"
+                    onClose={onClose}
+                >
+                    <Transition.Child
+                        as={Fragment}
+                        enter="ease-out duration-300"
+                        enterFrom="opacity-0"
+                        enterTo="opacity-100"
+                        leave="ease-in duration-200"
+                        leaveFrom="opacity-100"
+                        leaveTo="opacity-0"
+                    >
+                        <div
+                            className="fixed inset-0 bg-gray-500
+                            opacity-80 transition-opacity"
+                        />
+                    </Transition.Child>
+
+                    <div className="fixed inset-0 z-10 overflow-y-auto">
+                        
+                    </div>
+                </Dialog>
+            </Transition.Root>
         </>
     );
 };
