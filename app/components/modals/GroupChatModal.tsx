7227be3d989a7b566d71e08c89c1ac17bf24f17f
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-06-20 23:16:22 +0700

    update-Add-GroupChatModal

--- app/components/modals/GroupChatModal.tsx
+++ app/components/modals/GroupChatModal.tsx
@@ -31,10 +31,48 @@ const GroupChatModal: React.FC<GroupChatModalProps> = ({
     const router = useRouter();
     const [isLoading, setIsLoading] = useState(false);
 
+    const {
+        register,
+        handleSubmit,
+        setValue,
+        watch,
+        formState: {
+            errors,
+        },
+    } = useForm<FieldValues>({
+        defaultValues: {
+            name: '',
+            members: [],
+        },
+    });
+
+    const members = watch('members');
+
+    const onSubmit: SubmitHandler<FieldValues> = (data) => {
+
+    };
+
     return (
         <>
             <Modal isOpen={isOpen} onClose={onClose}>
-                
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <div className="space-y-12">
+                        <div className="border-b border-gray-900/10 pb-12">
+                            <h2
+                                className="text-base font-semibold
+                                leading-7 text-gray-900"
+                            >
+                                Create a group chat
+                            </h2>
+                            <p
+                                className="mt-1 text-sm
+                                leading-6 text-gray-600"
+                            >
+                                Create a chat with more than 2 people.
+                            </p>
+                        </div>
+                    </div>
+                </form>
             </Modal>
         </>
     );
