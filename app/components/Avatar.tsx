7f0ac032aca1c243c0c5a9d1dda05a0a8a5d64f3
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-07-16 23:35:25 +0700

    update-Add-StatusAvt

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -29,13 +29,13 @@ const Avatar: React.FC<AvatarProps> = ({ user }) => {
                         sizes="40"
                     />
                 </div>
-                {/* {isActive ? ( */}
+                {isActive ? (
                     <span
                         className="absolute block rounded-full
                         bg-green-500 ring-2 ring-white
                         top-0 right-0 h-2 w-2 md:h-3 md:w-3"
                     />
-                {/* ) : null} */}
+                ) : null}
             </div>
         </>
     );
